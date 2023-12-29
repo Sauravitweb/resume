1a5dc1544db4e95d316655ac1e210cc94d289fd5
Sauravitweb <155023084+Sauravitweb@users.noreply.github.com>
2023-12-29 14:10:48 +0545

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.querySelector('.ham').addEventListener("click", ()=>{
         document.querySelector('.ham').style.display = 'inline'
         document.querySelector('.cross').style.display = 'none'
     }
-    El se{
+    {
     
         document.querySelector('.cross').style.display = 'inline'
     
