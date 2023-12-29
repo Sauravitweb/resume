a0682148151b392065e2f4ca465517f3c3f26c0c
Sauravitweb <155023084+Sauravitweb@users.noreply.github.com>
2023-12-29 19:54:07 +0545

    Update script.js
    Sidebar

--- script.js
+++ script.js
@@ -17,3 +17,14 @@ document.querySelector('.ham').addEventListener("click", ()=>{
 
 }
 )
+function opensidebar() {
+  document.getElementById("sidebar").style.width = "250px";
+  document.querySelector(".ham").style.display = "none";
+  document.querySelector(".cross").style.display = "block";
+}
+
+function closesidebar() {
+  document.getElementById("sidebar").style.width = "0";
+  document.querySelector(".ham").style.display = "block";
+  document.querySelector(".cross").style.display = "none";
+}
