c64523c4e9dbf6396c5033945637373d4ad3beed
Sauravitweb <155023084+Sauravitweb@users.noreply.github.com>
2023-12-29 19:44:22 +0545

    Update script.js
    Looks

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ document.querySelector('.cross').style.display = 'none';
 document.querySelector('.ham').addEventListener("click", ()=>{
     document.querySelector('.sidebar').classList.toggle('sidebargo');
     if(document.querySelector('.sidebar').classList.contains('sidebargo')){
-        document.querySelector('.ham').style.display = 'inline'
-        document.querySelector('.cross').style.display = 'none'
+        document.querySelector('.ham').style.display = 'inline';
+        document.querySelector('.cross').style.display = 'none';
     }
    else {
     
-        document.querySelector('.cross').style.display = 'inline'
+        document.querySelector('.cross').style.display = 'inline';
     
-        document.querySelector('.ham').style.display = 'none'
+        document.querySelector('.ham').style.display = 'none';
 }
 
 
