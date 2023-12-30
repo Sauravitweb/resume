fb36f7e01b8107adeced1176146a56f4d514bea2
Sauravitweb <155023084+Sauravitweb@users.noreply.github.com>
2023-12-30 13:30:24 +0545

    Update script.js
    Curt

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-
-function opensidebar() {
-  document.getElementById("sidebar").style.width = "250px";
-  document.querySelector(".ham").style.display = "none";
-  document.querySelector(".cross").style.display = "block";
+console.log("script running.......")
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.ham').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.ham').style.display = 'inline';
+        document.querySelector('.cross').style.display = 'none';
+    }
+   else {
+    
+        document.querySelector('.cross').style.display = 'inline';
+    
+        document.querySelector('.ham').style.display = 'none';
 }
-
-function closesidebar() {
-  document.getElementById("sidebar").style.width = "0";
-  document.querySelector(".ham").style.display = "block";
-  document.querySelector(".cross").style.display = "none";
 }
+)
+
