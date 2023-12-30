11a20fbcc1d170822cbc86d0e93b8c6b6039c6c6
Sauravitweb <155023084+Sauravitweb@users.noreply.github.com>
2023-12-30 13:29:20 +0545

    Update script.js
    Cut

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-console.log("script running.......")
-document.querySelector('.cross').style.display = 'none';
-document.querySelector('.ham').addEventListener("click", ()=>{
-    document.querySelector('.sidebar').classList.toggle('sidebargo');
-    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
-        document.querySelector('.ham').style.display = 'inline';
-        document.querySelector('.cross').style.display = 'none';
-    }
-   else {
-    
-        document.querySelector('.cross').style.display = 'inline';
-    
-        document.querySelector('.ham').style.display = 'none';
-}
-}
-)
+
 function opensidebar() {
   document.getElementById("sidebar").style.width = "250px";
   document.querySelector(".ham").style.display = "none";
