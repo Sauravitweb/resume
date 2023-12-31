0a50b1367b20392a86ce1fe1296d67e3e35636ab
Sauravitweb <155023084+Sauravitweb@users.noreply.github.com>
2023-12-31 21:59:09 +0545

    Update script.js
    Cncnc

--- script.js
+++ script.js
@@ -30,7 +30,25 @@ document.querySelector('.port').addEventListener("click", ()=>{
 }
 }
 )
+function fillSkill(skillId, percentage) {
+    const fillBar = document.getElementById(skillId);
+    fillBar.style.width = `${percentage}%`;
+  }
 
+  // Example: Fill HTML skill to 80% after 2 seconds
+  setTimeout(function() {
+    fillSkill('htmlSkill', 80);
+  }, 2000);
+
+  // Example: Fill CSS skill to 70% after 4 seconds
+  setTimeout(function() {
+    fillSkill('cssSkill', 70);
+  }, 4000);
+
+  // Example: Fill JavaScript skill to 90% after 6 seconds
+  setTimeout(function() {
+    fillSkill('jsSkill', 90);
+  }, 6000);
 
 
 
