75b0fd5da872dc6acef81a539a7346127b425452
Sauravitweb <155023084+Sauravitweb@users.noreply.github.com>
2024-01-04 13:27:56 +0545

    Update script.js
    Vjvjv

--- script.js
+++ script.js
@@ -31,5 +31,19 @@ document.querySelector('.port').addEventListener("click", ()=>{
 }
 )
 
+              function httpGetAsync(url, callback) {
+                  const xmlHttp = new XMLHttpRequest();
+                  xmlHttp.onreadystatechange = function() {
+                      if (xmlHttp.readyState === 4 && xmlHttp.status === 200)
+                      callback(xmlHttp.responseText);
+                  }
+                  xmlHttp.open("GET", url, true); // true for asynchronous
+                  xmlHttp.send(null);
+              }
+
+              const url = "https://ipgeolocation.abstractapi.com/v1/?api_key=af41d0dbf1474f6b8be478699fd331a6&ip_address=27.34.17.224"
+
+              httpGetAsync(url)
+
 
 
